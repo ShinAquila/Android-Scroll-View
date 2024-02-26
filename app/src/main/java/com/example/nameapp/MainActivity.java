da5a096c2ef1801d26a0cf7fe13f3b7d229bc081
ShinAquila <augusto08rivera12@gmail.com>
2024-02-26 13:46:20 +0800

    added button to the bottom idk

--- app/src/main/java/com/example/nameapp/MainActivity.java
+++ app/src/main/java/com/example/nameapp/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.nameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.next_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){openActivity2();}
+
+
+        });
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
